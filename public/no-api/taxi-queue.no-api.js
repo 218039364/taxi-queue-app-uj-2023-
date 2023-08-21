d80e9162d14d6f5cf4ba8bb5a92299570e2c7d23
Tebogo Mojela <tebogomojela01@gmail.com>
2023-08-21 15:51:05 +0200

    first commit

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -1,30 +1,63 @@
 document.addEventListener('alpine:init', () => {
 
 	Alpine.data('TaxiQueue', () => {
-
+               
 		return {
 			version: 'no-api-1.0',
-			joinQueue() {
+            queueLength = 0,
+			
+			 class Queue  {
+				constructor() {
+				this.items = [];
+			   }
+
+
+			joinQueue(item) {
+				this.item.push(item);
 
 			},
+
+
 			leaveQueue() {
+            if (this.isEmpty()){
+				return "Queue is empty";
+			}
+			return this.items.shift();
+			}
+			isEmpty() {
+				return this.items.length === 0;
+			}
+			size() {
+			return	this.items.length;
+			}
 
+			front(){
+        if(this.isEmpty()) {
+			return "Queue is empty"
+		}
+		return this.items[0];
+			}
+		},
+
+		function	joinTaxiQueue(taxiQueue, taxiNumber) {
+             taxiQueue.enqueue(`Taxi ${taxiNumber}`);
 			},
 
-			joinTaxiQueue() {
-
+		function	queueLength(queue) {
+             return queue.size();
 			},
 
-			queueLength() {
-
+			function taxiQueueLength(queue) {
+             return queue.size();
 			},
 
-			taxiQueueLength() {
-
-			},
-
-			taxiDepart() {
-
+		function	taxiDepart(taxiQueue, peopleQueue, taxisLeaving, peopleLeaving) {
+        for (let = 0; i< taxisLeaving; i++) {
+			taxiQueue.dequeue();
+		}
+		for (let i=0; i < peopleLeaving; i++) {
+			peopleQueue.dequeue();
+		}
 			}
 		}
 
